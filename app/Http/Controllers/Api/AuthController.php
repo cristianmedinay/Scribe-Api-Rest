00e21b3ffa6eeafc4234ddb312889460b63ddcb6
cristian <cristianmedinay@gmail.com>
2023-09-22 01:53:32 +0200

    login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,33 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Api\AuthRequest;
+use Dingo\Api\Routing\Helpers;
+use \Dingo\Api\Http\Response;
 
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
 class AuthController extends Controller
 {
-    //
+    //send response
+    use Helpers;
+
+    public function login(AuthRequest $request):  Response  {
+        $credentials = request(['email',"password"]);
+        if(!$token = JWTAuth::attempt($credentials)){
+            $this->response->error(
+                "Credenciales Incorrectas",
+                 Response::HTTP_UNAUTHORIZED
+            );
+        }
+        return $this->respondWithToken($token);
+    }
+
+    protected function respondWithToken($token): Response {
+        return $this->response->array([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        /*     'expires_in_minutes' => auth('api')->factory()->getTTL(), */
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,17 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+$api = app('Dingo\Api\Routing\Router');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+
+$api->version('v1', function (Dingo\Api\Routing\Router $api) {
+    $api->post('/auth/login', [AuthController::class, 'login']);
+ 
+});
+
+
+
+/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+ */
